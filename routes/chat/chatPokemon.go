9f125d5b96829991ad2494333c98c79f42ef7915
Siddhant Vij <siddhantvij201@gmail.com>
2024-09-09 18:31:29 +0530

    refactor: prep for chats feat.

--- routes/chat/chatPokemon.go
+++ routes/chat/chatPokemon.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/siddhant-vij/PokeChat-Universe/cmd/web/templates/pages"
 	"github.com/siddhant-vij/PokeChat-Universe/config"
+	"github.com/siddhant-vij/PokeChat-Universe/controllers/pokedex"
 	"github.com/siddhant-vij/PokeChat-Universe/controllers/pokedex/utils"
 )
 
@@ -18,6 +19,19 @@ func ChatWithPokemonHandler(w http.ResponseWriter, r *http.Request, pokemonName 
 		serverErrorPage.Render(r.Context(), w)
 		return
 	}
-	chatPokemon := pages.PokedexChatPokemonPage(pokemon)
+
+	isCollectedParams := pokedex.IsPokemonCollectedParams{
+		UserID:    cfg.LoggedInUserId,
+		PokemonID: int32(pokemon.ID),
+	}
+	isCollected, err := cfg.DBQueries.IsPokemonCollected(context.Background(), isCollectedParams)
+	if err != nil {
+		log.Println(err)
+		serverErrorPage := pages.ServerErrorPage(cfg.AuthStatus)
+		serverErrorPage.Render(r.Context(), w)
+		return
+	}
+
+	chatPokemon := pages.PokedexChatPokemonPage(pokemon, isCollected)
 	chatPokemon.Render(r.Context(), w)
 }
